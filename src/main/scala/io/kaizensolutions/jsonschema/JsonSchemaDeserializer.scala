519c4e080d6ca09ff917e05fc70abce12e3f1ec7
A. Nakos <anakos@williamhill.us>
2023-04-12 05:26:40 -0700

    run scalafmt

--- src/main/scala/io/kaizensolutions/jsonschema/JsonSchemaDeserializer.scala
+++ src/main/scala/io/kaizensolutions/jsonschema/JsonSchemaDeserializer.scala
@@ -16,29 +16,29 @@ import java.nio.ByteBuffer
 import scala.reflect.ClassTag
 
 // See AbstractKafkaJsonSchemaDeserializer
-object JsonSchemaDeserializer   {
+object JsonSchemaDeserializer {
   def forValue[F[_]: Sync, A: Decoder](
     settings: JsonSchemaDeserializerSettings,
     client: SchemaRegistryClient
   )(implicit jsonSchema: json.Schema[A], tag: ClassTag[A]): F[ValueDeserializer[F, A]] =
     toJsonSchema(jsonSchema, settings.jsonSchemaId)
-      .flatMap( schema => forValue(settings, client, schema))
+      .flatMap(schema => forValue(settings, client, schema))
 
-  def forValue[F[_] : Sync, A: Decoder](
+  def forValue[F[_]: Sync, A: Decoder](
     settings: JsonSchemaDeserializerSettings,
     client: SchemaRegistryClient,
     schema: JsonSchema
   ): F[ValueDeserializer[F, A]] =
-    Ref.of[F, Set[Int]](Set.empty[Int])
-      .map { cache =>
-        val objectMapper = Jackson.newObjectMapper().configure(
+    Ref.of[F, Set[Int]](Set.empty[Int]).map { cache =>
+      val objectMapper = Jackson
+        .newObjectMapper()
+        .configure(
           DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
           settings.failOnUnknownKeys
         )
 
-        new JsonSchemaDeserializer[F, A](settings, schema, objectMapper, cache, client)
-          .jsonSchemaDeserializer
-      }
+      new JsonSchemaDeserializer[F, A](settings, schema, objectMapper, cache, client).jsonSchemaDeserializer
+    }
 }
 private class JsonSchemaDeserializer[F[_]: Sync, A](
   settings: JsonSchemaDeserializerSettings,
@@ -53,11 +53,11 @@ private class JsonSchemaDeserializer[F[_]: Sync, A](
   def jsonSchemaDeserializer: Deserializer[F, A] =
     Deserializer.instance { (_, _, bytes) =>
       Sync[F].delay {
-        val buffer             = getByteBuffer(bytes)
-        val id                 = buffer.getInt()
-        val serverSchema       = client.getSchemaById(id).asInstanceOf[JsonSchema]
-        val bufferLength       = buffer.limit() - 1 - IdSize
-        val start              = buffer.position() + buffer.arrayOffset()
+        val buffer       = getByteBuffer(bytes)
+        val id           = buffer.getInt()
+        val serverSchema = client.getSchemaById(id).asInstanceOf[JsonSchema]
+        val bufferLength = buffer.limit() - 1 - IdSize
+        val start        = buffer.position() + buffer.arrayOffset()
         val jsonNode: JsonNode =
           objectMapper.readTree(new ByteArrayInputStream(buffer.array, start, bufferLength))
 
@@ -78,8 +78,8 @@ private class JsonSchemaDeserializer[F[_]: Sync, A](
 
         check.as(jacksonToCirce(jsonNode))
       }
-      .map(decoder.decodeJson)
-      .rethrow
+        .map(decoder.decodeJson)
+        .rethrow
     }
 
   private def getByteBuffer(payload: Array[Byte]): ByteBuffer = {
@@ -92,7 +92,7 @@ private class JsonSchemaDeserializer[F[_]: Sync, A](
   private def checkSchemaCompatibility(serverSubjectId: Int, serverSchema: JsonSchema): F[Unit] = {
     val checkSchemaUpdateCache =
       Sync[F].delay {
-      val incompatibilities = clientSchema.isBackwardCompatible(serverSchema)
+        val incompatibilities = clientSchema.isBackwardCompatible(serverSchema)
         if (!incompatibilities.isEmpty)
           throw new IOException(
             s"Incompatible consumer schema with server schema: ${incompatibilities.toArray.mkString(", ")}"
--- src/main/scala/io/kaizensolutions/jsonschema/JsonSchemaSerializer.scala
+++ src/main/scala/io/kaizensolutions/jsonschema/JsonSchemaSerializer.scala
@@ -19,61 +19,62 @@ import scala.reflect.ClassTag
 import scala.jdk.OptionConverters._
 
 /**
- * Look at Confluent's KafkaJsonSchemaSerializer -> AbstractKafkaJsonSchemaSerializer -> AbstractKafkaSchemaSerDe
+ * Look at Confluent's KafkaJsonSchemaSerializer ->
+ * AbstractKafkaJsonSchemaSerializer -> AbstractKafkaSchemaSerDe
  *
  * The real implementation does the following (minus a few details):
  *
- * 1. call configure (provide isKey to figure out the subject name)
- *    - determine whether we automatically register the schema (involves using the subject name and the topic)
- *    - or determine whether we use the latest schema
- *    - determine whether to use strict compatibility
- *    - determine whether to validate the body against the payload and fail if not correct
- * 2. serialization of a message
- *    - extract the JSON Schema of the message (we don't need to do this every time because our Serializers are very specific)
- *    - register the schema (provided configuration) or use the latest schema
- *    - validate message against JSON schema (provided configuration)
- *    - write out message: magic byte ++ subject id ++ payload
+ *   1. call configure (provide isKey to figure out the subject name)
+ *      - determine whether we automatically register the schema (involves using
+ *        the subject name and the topic)
+ *      - or determine whether we use the latest schema
+ *      - determine whether to use strict compatibility
+ *      - determine whether to validate the body against the payload and fail if
+ *        not correct 2. serialization of a message
+ *      - extract the JSON Schema of the message (we don't need to do this every
+ *        time because our Serializers are very specific)
+ *      - register the schema (provided configuration) or use the latest schema
+ *      - validate message against JSON schema (provided configuration)
+ *      - write out message: magic byte ++ subject id ++ payload
  *
- *   see AbstractKafkaJsonSchemaDeserializer for deserialization details
+ * see AbstractKafkaJsonSchemaDeserializer for deserialization details
  */
 object JsonSchemaSerializer {
   final case class SubjectSchema(subject: String, schema: ParsedSchema)
 
   def forKey[F[_]: Sync, A: Encoder](
     settings: JsonSchemaSerializerSettings,
-    client: SchemaRegistryClient,
+    client: SchemaRegistryClient
   )(implicit jsonSchema: json.Schema[A], tag: ClassTag[A]): F[KeySerializer[F, A]] =
     toJsonSchema(jsonSchema, settings.jsonSchemaId)
       .flatMap(forKey[F, A](settings, client, _))
 
-  def forKey[F[_] : Sync, A: Encoder](
+  def forKey[F[_]: Sync, A: Encoder](
     settings: JsonSchemaSerializerSettings,
     client: SchemaRegistryClient,
-    schema: JsonSchema,
+    schema: JsonSchema
   ): F[KeySerializer[F, A]] =
-    Ref.of[F, Map[SubjectSchema, ParsedSchema]](Map.empty)
-      .map { cache =>
-        new JsonSchemaSerializer[F, A](client, settings, cache, schema)
-          .jsonSchemaSerializer(true)
-      }
+    Ref.of[F, Map[SubjectSchema, ParsedSchema]](Map.empty).map { cache =>
+      new JsonSchemaSerializer[F, A](client, settings, cache, schema)
+        .jsonSchemaSerializer(true)
+    }
 
   def forValue[F[_]: Sync, A: Encoder](
     settings: JsonSchemaSerializerSettings,
-    client: SchemaRegistryClient,
+    client: SchemaRegistryClient
   )(implicit jsonSchema: json.Schema[A], tag: ClassTag[A]): F[ValueSerializer[F, A]] =
     toJsonSchema(jsonSchema, settings.jsonSchemaId)
       .flatMap(forValue(settings, client, _))
 
-  def forValue[F[_] : Sync, A: Encoder](
+  def forValue[F[_]: Sync, A: Encoder](
     settings: JsonSchemaSerializerSettings,
     client: SchemaRegistryClient,
-    schema: JsonSchema,
+    schema: JsonSchema
   ): F[ValueSerializer[F, A]] =
-    Ref.of[F, Map[SubjectSchema, ParsedSchema]](Map.empty)
-      .map { cache =>
-        new JsonSchemaSerializer[F, A](client, settings, cache, schema)
-          .jsonSchemaSerializer(false)
-      }
+    Ref.of[F, Map[SubjectSchema, ParsedSchema]](Map.empty).map { cache =>
+      new JsonSchemaSerializer[F, A](client, settings, cache, schema)
+        .jsonSchemaSerializer(false)
+    }
 }
 
 private final class JsonSchemaSerializer[F[_]: Sync, A: Encoder](
@@ -112,16 +113,16 @@ private final class JsonSchemaSerializer[F[_]: Sync, A: Encoder](
         schema <- fSchema
         _      <- validatePayload(schema, jsonPayload)
         id     <- fId
-        bytes  <- Sync[F].delay {
-                    val payloadBytes = objectWriter.writeValueAsBytes(jsonPayload)
-                    val baos         = new ByteArrayOutputStream()
-                    baos.write(MagicByte.toInt)
-                    baos.write(ByteBuffer.allocate(IdSize).putInt(id).array())
-                    baos.write(payloadBytes)
-                    val bytes        = baos.toByteArray
-                    baos.close()
-                    bytes
-                  }
+        bytes <- Sync[F].delay {
+                   val payloadBytes = objectWriter.writeValueAsBytes(jsonPayload)
+                   val baos         = new ByteArrayOutputStream()
+                   baos.write(MagicByte.toInt)
+                   baos.write(ByteBuffer.allocate(IdSize).putInt(id).array())
+                   baos.write(payloadBytes)
+                   val bytes = baos.toByteArray
+                   baos.close()
+                   bytes
+                 }
       } yield bytes
     }
   }
@@ -151,11 +152,11 @@ private final class JsonSchemaSerializer[F[_]: Sync, A: Encoder](
             metadata.getSchema,
             metadata.getReferences
           )
-        }.flatMap { 
+        }.flatMap {
           _.toScala match {
             case Some(x) =>
               Sync[F].pure(x)
-            case None    =>
+            case None =>
               Sync[F].delay(new JsonSchema(metadata.getSchema).validate()) >>
                 // successfully parsed the schema locally means that the client was not properly configured
                 Sync[F].raiseError[ParsedSchema](
@@ -175,21 +176,21 @@ private final class JsonSchemaSerializer[F[_]: Sync, A: Encoder](
   ): F[ParsedSchema] = {
     val ss = SubjectSchema(subject, schema)
     for {
-      map           <- cache.get
-      result         = map.get(ss)
+      map   <- cache.get
+      result = map.get(ss)
       latestVersion <- result match {
                          case Some(cached) => Sync[F].pure(cached)
                          case None         => fetchLatest(subject)
                        }
       // the latest version must be backwards compatible with the current schema
       // this does not test forward compatibility to allow unions
-      compatIssues   = latestVersion.isBackwardCompatible(schema)
-      _             <- Sync[F].whenA(latestCompatStrict && !compatIssues.isEmpty) {
-                         Sync[F].raiseError(
-                           new IOException(s"Incompatible schema: ${compatIssues.toArray.mkString(", ")}")
-                         )
-                      }
-      _             <- Sync[F].whenA(result.isEmpty) { cache.update(_ + (ss -> latestVersion)) }
+      compatIssues = latestVersion.isBackwardCompatible(schema)
+      _ <- Sync[F].whenA(latestCompatStrict && !compatIssues.isEmpty) {
+             Sync[F].raiseError(
+               new IOException(s"Incompatible schema: ${compatIssues.toArray.mkString(", ")}")
+             )
+           }
+      _ <- Sync[F].whenA(result.isEmpty)(cache.update(_ + (ss -> latestVersion)))
     } yield latestVersion
   }
 }
